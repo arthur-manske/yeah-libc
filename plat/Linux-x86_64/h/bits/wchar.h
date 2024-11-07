0a5bc13a19cd72225622f9fb5a8e563db8d272c6
Arthur de S. Manske <arthur-manske@proton.me>
2024-11-07 13:54:55 +0000

    ADD (Linux-x86_64): bits/wchar.h limits for `wchar_t` and `wint_t`
    - Included definitions for `WCHAR_MAX`, `WCHAR_MIN`, `WINT_MAX`, and `WINT_MIN`.

---
+++ plat/Linux-x86_64/h/bits/wchar.h
@@ -0,0 +1,9 @@
+#ifndef __BITS_WCHAR_H__
+#define __BITS_WCHAR_H__
+
+#define WCHAR_MAX (0xffffffffu+L'\0')
+#define WCHAR_MIN (0x00000000u+L'\0')
+#define WINT_MAX  (0xffffffffu) 
+#define WINT_MIN  (0x00000000u)
+
+#endif
